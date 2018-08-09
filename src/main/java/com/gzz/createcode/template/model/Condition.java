95c49753b6326c485f09e9f79bdded5b612fbba1
gzz <gzz>
2018-08-09 10:15:51 +0800

    增加builder支持

--- src/main/java/com/gzz/createcode/template/model/Condition.java
+++ src/main/java/com/gzz/createcode/template/model/Condition.java
@@ -13,11 +13,19 @@ public class Condition {
 		sb.append(Utils.bigImport(fList));
 		sb.append("\r\nimport lombok.Getter;");
 		sb.append("\r\nimport lombok.Setter;");
+		sb.append("\r\nimport lombok.AllArgsConstructor;");
+		sb.append("\r\nimport lombok.Builder;");
+		sb.append("\r\nimport lombok.NoArgsConstructor;");
+		sb.append("\r\nimport lombok.experimental.Accessors;");
 		sb.append("\r\n");
 		sb.append("\r\nimport com.dl.keep.common.util.base.BaseCondition;");
 		sb.append(Utils.classNote(auth, cName + "查询条件实体类"));
 		sb.append("\r\n@Setter");
 		sb.append("\r\n@Getter");
+		sb.append("\r\n@Accessors(chain = true)");
+		sb.append("\r\n@Builder");
+		sb.append("\r\n@AllArgsConstructor");
+		sb.append("\r\n@NoArgsConstructor");
 		sb.append("\r\npublic class " + clsUpp + "Cond extends BaseCondition {");
 		StringBuilder field = new StringBuilder();
 		StringBuilder cond = new StringBuilder();
@@ -28,7 +36,7 @@ public class Condition {
 			cond.append("\r\n\t\t//add(" + fName + ", \"AND t." + fName + (type.equals("String") ? " LIKE ?\", 3);" : " = ?\");"));
 		}
 		cond.append("\r\n\t\t//add(ids, \"AND t.id IN \");");
-		
+
 		sb.append(Utils.methodNote("拼加自定义条件 "));
 		sb.append("\r\n	@Override");
 		sb.append("\r\n	public void addCondition() { ");
@@ -38,7 +46,7 @@ public class Condition {
 		sb.append("\r\n\t//查询条件,把不用的条件清理掉");
 		sb.append(field);
 		sb.append("\r\n\t//private List<Long> ids;// 主键列表");
-		
+
 		sb.append("\r\n");
 		sb.append("\r\n}");
 		return sb;
