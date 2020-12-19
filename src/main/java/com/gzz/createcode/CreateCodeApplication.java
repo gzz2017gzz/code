4071909a880b1f7373c224a1166b628738ab2a47
gaozhenzhong <gzz_gzz@163.com>
2020-12-20 01:35:39 +0800

    测试一下

--- src/main/java/com/gzz/createcode/CreateCodeApplication.java
+++ src/main/java/com/gzz/createcode/CreateCodeApplication.java
@@ -3,6 +3,12 @@ package com.gzz.createcode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 我的代码生成器
+ * 
+ * @author Administrator
+ *
+ */
 @SpringBootApplication
 public class CreateCodeApplication {
 	public static void main(String[] args) {
